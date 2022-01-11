a0eeef911fd2d97c921c426befbe028b381cd871
locano <ludwing238@gmail.com>
2022-01-11 09:43:23 -0600

    adding react start app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const StartComponent = () => {
+  return <h1> New React APP</h1>;
+};
+
+ReactDOM.render(<StartComponent />, document.getElementById("root"));
